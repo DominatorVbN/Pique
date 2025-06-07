086bb35f633bb583f97f0e6ff890b4405deb916c
DominatorVbN <voilentbynature@gmail.com>
2025-06-07 19:23:47 +0800

    Initial Commit

---
+++ Pique/PiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiqueApp.swift
+//  Pique
+//
+//  Created by Amit Samant on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
